9f51c3c53ac8a985f1746b9afac6e7bd251b28d0
hidiegorguez <diego.rodgar@hotmail.com>
2024-11-12 13:43:57 +0100

    function app files

---
+++ function_app.py
@@ -0,0 +1,16 @@
+import logging
+import azure.functions as func
+import main
+import os
+
+app = func.FunctionApp()
+
+@app.schedule(schedule="0 0 3 * * *", arg_name="myTimer",
+              use_monitor=False) 
+
+def TodoistTimerTrigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+        
+    respDiego = main.mainDiego(address=os.getenv('EMAIL_DIEGO'))
+    logging.info(f'Diego execution: {respDiego}')
